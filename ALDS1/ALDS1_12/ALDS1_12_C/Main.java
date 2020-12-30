ca60e7a27cfa9ca482b32bd7d6202e25f3e24583
Acasune <tttt4aaaa@gmail.com>
2020-12-30 15:52:53 +0900

    [ADD]Solved ALDS1_12_C

---
+++ ALDS1/ALDS1_12/ALDS1_12_C/Main.java
@@ -0,0 +1,56 @@
+/*
+ *  ALDS1_11_B: Single Source Shortest Path Ⅰ
+ *  URL: https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_12_B
+ */
+
+package ALDS1.ALDS1_12.ALDS1_12_B;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int INF=Integer.MAX_VALUE/10;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] cost = new int[N][N];
+        for (int i=0;i<N;i++)for (int j=0;j<N;j++)cost[i][j]=INF;
+        for (int i=0;i<N;i++){
+            int u=sc.nextInt(), k=sc.nextInt();
+            for (int j=0;j<k;j++){
+                int v=sc.nextInt(), c=sc.nextInt();
+                cost[u][v]=c;
+            }
+        }
+
+        int res = 0;
+        boolean[] used = new boolean[N];
+        int[] minCost = new int[N];
+        Arrays.fill(used, false);
+        Arrays.fill(minCost,INF);
+        minCost[0] = 0;
+
+        while (true){
+            int v=-1;
+            for (int u=0; u<N;u++){
+                if(!used[u]&&(v==-1||minCost[u]<minCost[v]))v=u;
+            }
+
+            if(v==-1) break;
+            used[v]=true;
+
+            for (int u=0; u<N;u++){
+                minCost[u] = Integer.min(minCost[u],minCost[v]+cost[v][u]);
+            }
+
+        }
+
+        for (int i=0;i<N;i++){
+            System.out.println(i+" "+minCost[i]);
+        }
+
+    }
+
+}
