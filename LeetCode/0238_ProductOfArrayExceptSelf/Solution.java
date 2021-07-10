7bb1902b87753306caea4cbbc29d64ee5d19a81f
Acasune <tttt4aaaa@gmail.com>
2021-07-10 12:06:07 +0900

    Solve:0238_ProductOfArrayExceptSelf

--- LeetCode/0238_ProductOfArrayExceptSelf/Solution.java
+++ LeetCode/0238_ProductOfArrayExceptSelf/Solution.java
@@ -1,23 +1,24 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
   public int[] productExceptSelf(int[] nums) {
     int[] ans = new int[nums.length];
-    int cntZero = Arrays.stream(nums).filter(e -> e == 0).count();
+    long cntZero = Arrays.stream(nums).filter(e -> e == 0).count();
     if (cntZero >= 2) {
       return ans;
     }
 
-    int product = Arrays.stream(nums).filter(e -> e != 0).reduce(0, (a, b) -> a * b);
+    long product = Arrays.stream(nums).filter(e -> e != 0).reduce(1, (a, b) -> a * b);
     for (int i = 0; i < nums.length; i++) {
       if (cntZero == 1) {
-        if (nums[i] == 0) {
+        if (nums[i] != 0) {
           ans[i] = 0;
         } else {
-          ans[i] = product;
+          ans[i] = (int) product;
         }
       } else {
-        ans[i] = product / nums[i];
+        ans[i] = (int) (product / nums[i]);
       }
     }
     return ans;
