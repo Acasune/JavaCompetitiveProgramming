2d596bb1558682af5603ef21dd4a4b9d05e4c3c9
Acasune <tttt4aaaa@gmail.com>
2021-11-28 10:46:31 +0900

    Refactoring:0692

--- LeetCode/0692_TopKFrequentWords/Solution.java
+++ LeetCode/0692_TopKFrequentWords/Solution.java
@@ -1,5 +1,6 @@
 import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
@@ -8,20 +9,13 @@ class Solution {
   public List<String> topKFrequent(String[] words, int k) {
     LinkedHashMap<String, Integer> mp = new LinkedHashMap<>();
     for (String word : words) {
-      if (!mp.containsKey(word)) {
-        mp.put(word, 1);
-      } else {
-        mp.put(word, mp.get(word) + 1);
-      }
+      mp.put(word, mp.getOrDefault(word, 0) + 1);
     }
+    Comparator<Map.Entry<String, Integer>> comparator = Comparator.comparing(Map.Entry<String, Integer>::getValue)
+        .reversed().thenComparing(Map.Entry<String, Integer>::getKey);
 
-    var ret = mp.entrySet().stream().sorted((e1, e2) -> {
-      if (e1.getValue() != e2.getValue()) {
-        return -e1.getValue().compareTo(e2.getValue());
-      } else {
-        return e1.getKey().compareTo(e2.getKey());
-      }
-    }).limit(k).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    var ret = mp.entrySet().stream().sorted(comparator).limit(k)
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     return new ArrayList(ret.keySet());
   }
 }
