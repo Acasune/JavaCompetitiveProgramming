11f054b2c3dbb8c44563977bc74472b6f64bec6e
Acasune <tttt4aaaa@gmail.com>
2021-12-19 08:28:30 +0900

    Solve:0442

---
+++ LeetCode/0442_FindAllDuplicatesInAnArray/Solution.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+class Solution {
+  public List<Integer> findDuplicates(int[] nums) {
+    List<Integer> ans = new LinkedList<>();
+    for (int i = 0; i < nums.length; i++) {
+      int idx = Math.abs(nums[i]) - 1;
+      if (nums[idx] < 0) {
+        ans.add(Math.abs(nums[i]));
+      }
+      nums[idx] = -nums[idx];
+
+    }
+    return ans;
+  }
+}
