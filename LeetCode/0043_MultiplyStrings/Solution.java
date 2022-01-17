9efdae0398d918ae92e8c44499944e6662538eec
Acasune <tttt4aaaa@gmail.com>
2022-01-17 20:38:13 +0900

    Solve:0043

--- LeetCode/0043_MultiplyStrings/Solution.java
+++ LeetCode/0043_MultiplyStrings/Solution.java
@@ -1,22 +1,36 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Arrays;
 
 class Solution {
-  public int trap(int[] height) {
-    Deque<Integer> posDeq = new ArrayDeque<>();
-
-    int ret = 0, i = 0;
-    while (i < height.length) {
-      if (posDeq.isEmpty() || height[i] <= height[posDeq.peekLast()]) {
-        posDeq.addLast(i);
-        i += 1;
-      } else {
-        int bot = posDeq.pollLast();
-        int tmp = posDeq.isEmpty() ? 0
-            : (Math.min(height[posDeq.peekLast()], height[i]) - height[bot]) * (i - posDeq.peekLast() - 1);
-        ret += tmp;
+  public String multiply(String num1, String num2) {
+    char[] n1 = num1.toCharArray(), n2 = num2.toCharArray();
+    int digit1 = 0, digit2 = 0;
+    int l1 = n1.length, l2 = n2.length;
+    int l = l1 + l2;
+    int[] memo = new int[l1 + l2];
+    for (int i = l1 - 1; 0 <= i; i--) {
+      digit2 = 0;
+      for (int j = l2 - 1; 0 <= j; j--) {
+        memo[l - digit2 - digit1 - 1] += (n1[i] - '0') * (n2[j] - '0');
+        digit2++;
+      }
+      digit1++;
+    }
+    for (int i = l1 + l2 - 1; 0 < i; i--) {
+      if (memo[i] >= 10) {
+        memo[i - 1] += memo[i] / 10;
+        memo[i] = memo[i] % 10;
       }
     }
-    return ret;
+    int i = 0;
+    while (i < l1 + l2 - 1 && memo[i] == 0) {
+      i++;
+    }
+    StringBuilder ret = new StringBuilder();
+    for (int j = i; j < l1 + l2; j++) {
+      ret.append(memo[j]);
+    }
+
+    return ret.toString();
   }
+
 }
