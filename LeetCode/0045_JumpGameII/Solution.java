896a67609eb321bc33b1c83c51c337cfc85af0b3
Acasune <tttt4aaaa@gmail.com>
2022-01-17 20:50:08 +0900

    Solve:0045

---
+++ LeetCode/0045_JumpGameII/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+  public int jump(int[] nums) {
+    int n = nums.length;
+    int[] memo = new int[n];
+    Arrays.fill(memo, 100_000_000);
+    memo[0] = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 1; j <= nums[i]; j++) {
+        if (i + j >= n) {
+          break;
+        }
+        memo[i + j] = Math.min(memo[i + j], memo[i] + 1);
+      }
+    }
+    return memo[n - 1];
+
+  }
+}
